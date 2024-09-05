930cd1a80ea26717a347e5557f765f6580c41bca
Mangesh2004 <mangeshwaybhase01@gmail.com>
2024-09-05 23:06:23 +0530

    update

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -51,15 +51,7 @@ const Navbar = () => {
             About
             <span className="absolute left-0 bottom-0 w-full h-0 bg-black transition-all duration-300 group-hover:h-[2px]" />
           </ScrollLink>
-          <ScrollLink
-            to="footer"
-            smooth
-            duration={500}
-            className="relative inline-block px-4 py-2 text-black rounded group cursor-pointer"
-          >
-            Contact
-            <span className="absolute left-0 bottom-0 w-full h-0 bg-black transition-all duration-300 group-hover:h-[2px]" />
-          </ScrollLink>
+          
           <ScrollLink
             to="team"
             smooth
